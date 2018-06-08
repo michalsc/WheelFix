4e7a85e40395535053686b8c1cb5f0e5d68c3851
Michal Schulz <michal.schulz@tu-clausthal.de>
2018-06-08 16:23:30 +0200

    Initial Commit

---
+++ WheelFix/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  WheelFix
+//
+//  Created by Michal Schulz on 08.06.18.
+//  Copyright © 2018 Michal Schulz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
