38e010eaf31da1d6e94938b2de2007fd199ddd61
Michal Schulz <michal.schulz@gmx.de>
2018-06-10 23:10:26 +0200

    Update licence information in source files. Add some comments to WheelTapper class.

--- WheelFix/WheelTapper.swift
+++ WheelFix/WheelTapper.swift
@@ -5,29 +5,56 @@
 //  Created by Michal Schulz on 09.06.18.
 //  Copyright © 2018 Michal Schulz. All rights reserved.
 //
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 import Cocoa
 
+// Event filter callback. Here the event will be filtered out or processed
+// according to settings in the WheelTapper class
 func myCGEventCallback(proxy: CGEventTapProxy, type: CGEventType, event: CGEvent, refcon: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? {
 
+    // Extract WheelTapper instance from raw pointer in userData
     let tapper = Unmanaged<WheelTapper>.fromOpaque(refcon!).takeUnretainedValue()
 
+    // If wheel event is continuous, i.e. if it is pixel based
     if 0 != event.getIntegerValueField(.scrollWheelEventIsContinuous) {
+        // Get timestamp of the event and the movement delta
         let ts = event.timestamp
         let delta = event.getIntegerValueField(.scrollWheelEventPointDeltaAxis1)
+
+        // Shall spurious sign changes be filtered? If yes they must occur shortly enough
+        // after last event (shorter than inactivityDelay) but not be sent too quickly
+        // (time distance longer than continuousDelay).
         if tapper.ignoreSpuriousSigns {
             if ((ts - tapper.lastEventTS) < tapper.inactivityDelay) && ((ts - tapper.lastEventTS) > tapper.continuousDelay) {
+                // Test if delta movement sign has changed
                 if delta * tapper.lastEventDelta < 0 {
-                    debugPrint("unusual sign change in wheel, timestamp difference is %f ms. Skipping event", Double(ts - tapper.lastEventTS) / 1000000.0)
-                    debugPrint(delta)
+                    // Just discard the event completely, do not store timestamp or
+                    // delta of the event
                     return nil
                 }
             }
         }
+        // Update last event timestamp and movement delta
         tapper.lastEventTS = ts
         tapper.lastEventDelta = delta
+
+        // Place for further event manipulation...
     }
 
+    // Return event for further processing
     return Unmanaged.passRetained(event)
 }
 
